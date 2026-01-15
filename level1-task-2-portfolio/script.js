9103670e8e7b27c526f731b4e2ed3d5ccc533380
sec24ad122 <sec24ad122@sairamtap.edu.in>
2026-01-15 21:49:17 +0530

    Create script.js

---
+++ level1-task-2-portfolio/script.js
@@ -0,0 +1,11 @@
+// Portfolio JS
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert(
+            "Thank you for contacting me!\n" +
+            "Thanigai | Web Development Intern – Oasis Infobyte"
+        );
+    });
+});
+
+console.log("Portfolio loaded successfully");
